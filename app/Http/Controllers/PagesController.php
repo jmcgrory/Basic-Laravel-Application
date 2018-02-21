f907f6013e2fa7aa72356be1e4e8905e875c1c6f
Jamie McGrory <contact@jmcgr.co.uk>
2018-02-21 21:27:45 +0000

    post npm install

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,14 +7,21 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view('pages.index');
+        $title = 'Welcome to the Jungle';
+        // return view('pages.index', compact('title'));
+        return view('pages.index')->with('title', $title);
     }
     
     public function about(){
-        return view('pages.about');
+        $title = 'All About That Action';
+        return view('pages.about')->with('title', $title);
     }
 
     public function services(){
-        return view('pages.services');
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Gardening', 'DIY', 'Cleaning']
+        );
+        return view('pages.services')->with($data);
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'LSAPP')}}</title>
-    </head>
-    <body>
-        <h1>LSAPP</h1>
-        <p>Non sit enim eu occaecat in velit deserunt. Laborum consequat do elit dolore fugiat tempor culpa est. Amet laboris nisi aliquip eiusmod officia laborum ex qui. Duis adipisicing elit duis consequat.</p>
-    </body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h1>{{$title}}</h1>
+    <p>Non sit enim eu occaecat in velit deserunt. Laborum consequat do elit dolore fugiat tempor culpa est. Amet laboris nisi aliquip eiusmod officia laborum ex qui. Duis adipisicing elit duis consequat.</p>
+@endsection
